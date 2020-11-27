f72dfb9ba0036c0a7a83c4ac32b1ccdd304c4bdf
Fabian Ziegner <fz31dovo@studserv.uni-leipzig.de>
2020-11-27 12:04:52 +0100

    Added new parser + results.json

--- parser/parser.py
+++ parser/parser.py
@@ -2,39 +2,32 @@ import pandas as pd
 import json
 import re
 
-input = "14111-01-03-4.csv"
-with open(input, encoding='cp1252') as f:
+input = "btw17_kerg.csv"
+land_list = ["Schleswig-Holstein","Hamburg","Niedersachsen","Bremen","Nordrhein-Westfalen","Hessen","Rheinland-Pfalz","Baden-Württemberg",
+            "Bayern","Saarland","Berlin","Brandenburg","Mecklenburg-Vorpommern","Sachsen","Sachsen-Anhalt","Thüringen","Bundesgebiet"]
+
+test = []
+with open(input, mode = "r", encoding="utf-8") as f:
     conv =  []
     for line in f:
-        if(re.match("\d\d.\d\d.\d\d\d\d;\d", line)):
+        if re.match("(\d)?(\d)?\d;", line):
             line = re.sub("\n","",line)
+            test.append(line)
             split = line.split(";")
-            if len(split[1]) == 5 or str(split[1]) in ["02","04","11"]:
+            if split[1] not in land_list: #filter Bundesland/Bundesgebiet results
                 conv.append(split)
-
-dict = {"01":"Schleswig-Holstein",
-        "02":"Hamburg",
-        "03":"Niedersachsen",
-        "04":"Bremen",
-        "05":"Nordrhein-Westfalen",
-        "06":"Hessen",
-        "07":"Rheinland-Pfalz",
-        "08":"Baden-Württemberg",
-        "09":"Bayern",
-        "10":"Saarland",
-        "11":"Berlin",
-        "12":"Brandenburg",
-        "13":"Mecklenburg-Vorpommern",
-        "14":"Sachsen",
-        "15":"Sachsen-Anhalt",
-        "16":"Thüringen"}
 
 df = pd.DataFrame(conv)
 
 with open("results.json", mode='w', encoding="utf-8") as feedsjson:
-    list = []
+    content = []
     for index, row in df.iterrows():
-        land_id = row[1][0:2]
-        entry = {'date':row[0],'bundesland':dict[land_id],"name":row[2].strip().split(",")[0],"eligible_voters":row[3],"turnout":row[4],"voters":row[5],"cdu_csu":row[6],"spd":row[7],"gruene":row[8],"fdp":row[9],"linke":row[10],"afp":row[11],"misc":row[12]}
-        list.append(entry)
-    json.dump(list, feedsjson)
+        if(row[2] == "9"): #if Wahlkreis in Bayern (ID = 9) choose CSU
+            union = row[37]
+        else: #else choose CDU
+            union = row[21]
+        not_misc = [union,row[25],row[33],row[45],row[29],row[41]]
+        misc = int(row[17]) - sum(int(votes) for votes in not_misc)
+        entry = {"WKR_NR":row[0],"WKR_NAME":row[1],"eligible_voters":row[2],"union":union,"spd":row[25],"gruene":row[33],"afd":row[45],"linke":row[29],"fdp":row[41],"misc":str(misc)}
+        content.append(entry)
+    json.dump(content, feedsjson)
