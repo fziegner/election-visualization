7b9658952abaa781cfd5baaeaa85b91749bb55d8
Steven Lehmann <sl74byra@studserv.uni-leipzig.de>
2021-01-03 11:39:18 +0100

    Added combined results for btw 05,02 & 98 and updated matcher. Some missing values in geojson result files

--- matcher/matcher.py
+++ matcher/matcher.py
@@ -4,9 +4,10 @@ import geojson
 
 from Levenshtein import distance
 
-FILE_PATH_GEOJSON = 'wahlkreise.geojson'
-FILE_PATH_RESULTS = "results.json"
+FILE_PATH_GEOJSON = '1998GeometrieWahlkreise14DBT0.geojson'
+FILE_PATH_RESULTS = "btw98_results.json"
 
+OUTPUT_PATH = "F:/Repository/wcmpraktikum/Datasets/Combined/btw_98.geojson"
 
 with open(FILE_PATH_GEOJSON, encoding='utf-8') as f:
     gj = geojson.load(f)
@@ -15,33 +16,55 @@ with open(FILE_PATH_RESULTS, encoding='utf-8') as f:
     resultsjson = json.load(f)
 
 
+def getWKR(wkr_name):
+    results = [res for res in resultsjson]
+    candidates = []
+
+    for wkr in results:
+        if(wkr['WKR_NAME'] == wkr_name):
+            return wkr
+
+        if distance(wkr['WKR_NAME'], wkr_name) <= 2:
+            candidates.append(wkr)
+
+
+    if len(candidates) != 0:
+        variable=1
+        if variable == '':
+            variable = 1
+        return candidates[int(variable)-1]
+    else:
+        r = {'WKR_NR': '', 'WKR_NAME':'', 'eligible_voters':'', 'union':'', 'spd':'', 'gruene':'', 'afd':'', 'linke':'', 'fdp':'', 'misc':''}
+        return r
+
 def main():
     wkr_names = [i for i in gj['features']]
     results = [res for res in resultsjson]
 
-
-
-    with open('wahlkreise_results_2018.geojson', "w") as f:
+    with open(OUTPUT_PATH, "w") as f:
         new = {}
         new['type'] = gj['type']
         new['crs'] = gj['crs']
         new['features'] = []
-
+        mapping = {}
         i=0
         for wkr in wkr_names:
+            r = getWKR(wkr['properties']['WKR_NAME'])
+            mapping[wkr['properties']['WKR_NAME']] = getWKR(wkr['properties']['WKR_NAME'])['WKR_NAME']
+
             gj_feature = {}
             gj_feature['type'] = wkr['type']
             gj_feature['properties'] = wkr['properties']
             gj_feature['geometry'] = wkr['geometry']
 
-            gj_feature['properties']['eligible_voters'] = results[i].get('eligible_voters')
-            gj_feature['properties']['union'] = results[i].get('union')
-            gj_feature['properties']['spd'] = results[i].get('spd')
-            gj_feature['properties']['gruene'] = results[i].get('gruene')
-            gj_feature['properties']['afd'] = results[i].get('afd')
-            gj_feature['properties']['linke'] = results[i].get('linke')
-            gj_feature['properties']['fdp'] = results[i].get('fdp')
-            gj_feature['properties']['misc'] = results[i].get('misc')
+            gj_feature['properties']['eligible_voters'] = r['eligible_voters']
+            gj_feature['properties']['union'] = r['union']
+            gj_feature['properties']['spd'] = r['spd']
+            gj_feature['properties']['gruene'] = r['gruene']
+            gj_feature['properties']['afd'] = r['afd']
+            gj_feature['properties']['linke'] = r['linke']
+            gj_feature['properties']['fdp'] = r['fdp']
+            gj_feature['properties']['misc'] = r['misc']
             i+=1
 
             new['features'].append(gj_feature)
