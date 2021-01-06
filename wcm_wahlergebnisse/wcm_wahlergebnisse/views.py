9821efbbd69e1c0abf90ec04fa86ecdc0b29561d
Smunfr <christian.schlecht97@web.de>
2021-01-06 11:33:32 +0100

    implement build box to control the map (only show immediate not yet implemented)

--- wcm_wahlergebnisse/wcm_wahlergebnisse/views.py
+++ wcm_wahlergebnisse/wcm_wahlergebnisse/views.py
@@ -5,7 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 import json
 
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from wcm_wahlergebnisse import app
 
@@ -13,7 +13,7 @@ from wcm_wahlergebnisse import app
 @app.route('/home')
 def home():
     """Renders the home page."""
-    with open("wcm_wahlergebnisse/wahlkreise_results_2018.geojson", "r") as fp:
+    with open("../Datasets/Combined/btw_17.geojson", "r") as fp:
         data = json.load(fp)
     return render_template(
         'index.html',
@@ -21,6 +21,29 @@ def home():
         year=datetime.now().year,
         wahlkreise=data
     )
+@app.route("/data")
+def data():
+    """
+    request data:
+        query params: type = any of btw, ew, ltw, kw
+                      year = any year
+
+        responses:
+            200 --> data
+            400 --> combination of type and data not found
+    """
+    type = request.args.get("type", type=str)  # any of btw, ew, ltw, kw
+    year = request.args.get("year", type=str)
+    try:
+        filepath = "../Datasets/Combined/" + type + "_" + str(year) + ".geojson"
+        print(filepath)
+        with open(filepath, "r") as fp:
+            data = json.load(fp)
+    except OSError:
+        return jsonify({"reason": "data_not_found"}), 400
+
+    return jsonify(data), 200
+
 
 @app.route('/contact')
 def contact():
