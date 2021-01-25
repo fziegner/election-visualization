5628ca1194b2f37b79c7b317a5c609d8d9ddabf2
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2021-01-25 15:22:59 +0100

    Updated matcher for EW, Try 2

--- matcher/ew_matcher.py
+++ matcher/ew_matcher.py
@@ -1,13 +1,18 @@
 import os
 import json
 import geojson
+from difflib import SequenceMatcher
 
 from Levenshtein import distance
 
-FILE_PATH_GEOJSON = 'test2.geojson'
-FILE_PATH_RESULTS = "ew04_results.json"
+FILE_PATH_GEOJSON = "landkreise_simplify20_2009.geojson"
+FILE_PATH_RESULTS = "../Datasets/Wahlergebnisse/ew09_results.json"
 
-OUTPUT_PATH = "ew_2004.geojson"
+OUTPUT_PATH = "../Datasets/Combined/ew_2009.geojson"
+
+MODE = OUTPUT_PATH.split("/")[-1].split("_")[0]
+YEAR = int(OUTPUT_PATH.split("_")[1][:4])
+print(YEAR)
 
 land_dict = {
     "01": "Schleswig-Holstein",
@@ -27,6 +32,10 @@ land_dict = {
     "15": "Sachsen-Anhalt",
     "16": "Thüringen"
 }
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 with open(FILE_PATH_GEOJSON, encoding='utf-8') as f:
     gj = geojson.load(f)
@@ -58,43 +67,85 @@ def getWKR(wkr_name):
 def getData(id):
     results = [res for res in resultsjson]
     for data in results:
-        if(data["WKR_NR"] == id):
+        if data["WKR_NR"].replace(" ", "") == id:
             return data
 
 def main():
     wkr_names = [i for i in gj['features']]
+    #print(wkr_names)
     results = [res for res in resultsjson]
 
     with open(OUTPUT_PATH, "w") as f:
-        new = {}
-        new['type'] = gj['type']
-        new['crs'] = gj['crs']
-        new['features'] = []
-        mapping = {}
-        i=0
+        new = {'features': []}
+        i = 0
         for wkr in wkr_names:
-            id = wkr["properties"]["RS"]
-            r = getData(id)
-            if r:
+            if YEAR in [2019, 2009, 2014]:
+                new['type'] = gj['type']
+                new['crs'] = gj['crs']
+                new["parameters"] = {"election_type": MODE, "election_year": YEAR}
+                id_ = wkr["properties"]["RS"].replace(" ", "")
+                print(id_)
+                r = getData(id_)
+                if YEAR in [2009, 2014]:
+                    if id_.startswith("0"):
+                        r = getData(id_[1:])
+                print(r)
+                if r:
+                    gj_feature = {}
+                    gj_feature['type'] = wkr['type']
+                    gj_feature['properties'] = wkr['properties']
+                    gj_feature['geometry'] = wkr['geometry']
+
+                    gj_feature['properties']['WKR_NAME'] = r['WKR_NAME']
+                    gj_feature['properties']['LAND_NAME'] = land_dict[id_[0:2]]
+                    gj_feature['properties']['eligible_voters'] = r['total_votes'] # !!! I changed eligible_voters to total_votes in the parser of the EW files !!!
+                    gj_feature['properties']['union'] = r['union']
+                    gj_feature['properties']['spd'] = r['spd']
+                    gj_feature['properties']['gruene'] = r['gruene']
+                    gj_feature['properties']['afd'] = r['afd']
+                    gj_feature['properties']['linke'] = r['linke']
+                    gj_feature['properties']['fdp'] = r['fdp']
+                    gj_feature['properties']['misc'] = r['misc']
+                    i += 1
+
+                    new['features'].append(gj_feature)
+            else:
+                new["parameters"] = {"election_type": MODE, "election_year": YEAR}
+                gj_name = wkr["properties"]["NAME_3"].replace(" Städte", ", Stadt")
+                match = False
+                save = None
+                for res in resultsjson:
+                    res_name = res["WKR_NAME"].split(",")[0]
+                    if gj_name == res_name:
+                        match = True
+                        save = res
+                if not match:
+                    sim = 0
+                    for res in resultsjson:
+                        res_name = res["WKR_NAME"].split(",")[0]
+                        if similar(gj_name, res_name) > sim:
+                            sim = similar(gj_name, res_name)
+                            save = res
                 gj_feature = {}
                 gj_feature['type'] = wkr['type']
                 gj_feature['properties'] = wkr['properties']
                 gj_feature['geometry'] = wkr['geometry']
 
-                gj_feature['properties']['WKR_NAME'] = r['WKR_NAME']
-                gj_feature['properties']['LAND_NAME'] = land_dict[id[0:2]]
-                gj_feature['properties']['eligible_voters'] = r['total_votes'] # !!! I changed eligible_voters to total_votes in the parser of the EW files !!!
-                gj_feature['properties']['union'] = r['union']
-                gj_feature['properties']['spd'] = r['spd']
-                gj_feature['properties']['gruene'] = r['gruene']
-                gj_feature['properties']['afd'] = r['afd']
-                gj_feature['properties']['linke'] = r['linke']
-                gj_feature['properties']['fdp'] = r['fdp']
-                gj_feature['properties']['misc'] = r['misc']
-                i+=1
+                gj_feature['properties']['WKR_NAME'] = save['WKR_NAME']
+                gj_feature['properties']['LAND_NAME'] = wkr["properties"]["NAME_1"]
+                gj_feature['properties']['eligible_voters'] = save['total_votes']  # !!! I changed eligible_voters to total_votes in the parser of the EW files !!!
+                gj_feature['properties']['union'] = save['union']
+                gj_feature['properties']['spd'] = save['spd']
+                gj_feature['properties']['gruene'] = save['gruene']
+                gj_feature['properties']['afd'] = save['afd']
+                gj_feature['properties']['linke'] = save['linke']
+                gj_feature['properties']['fdp'] = save['fdp']
+                gj_feature['properties']['misc'] = save['misc']
 
                 new['features'].append(gj_feature)
 
         f.write(json.dumps(new))
+
+
 if __name__ == "__main__":
     main()
